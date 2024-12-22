ae98107b709b317245a6b25ec8b5c625f6e3e941
dikobr <leshik.cookie@gmail.com>
2024-12-22 10:12:59 +0300

    first commit

---
+++ news/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum
+from django.db.models.functions import Coalesce
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.FloatField(default=0)
+
+    def update_rating(self):
+        posts_rating = self.posts.aggregate(pr=Coalesce(Sum('rating'), 0))['pr']
+        comments_rating = self.user.comments.aggregate(cr=Coalesce(Sum('rating'), 0))['cr']
+        posts_comments_rating = self.posts.aggregate(pcr=Coalesce(Sum('comment__rating'), 0))['pcr']
+
+        self.rating = posts_rating * 3 + comments_rating + posts_comments_rating
+        self.save()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Post(models.Model):
+    ARTICLE = 'AR'
+    NEWS = 'NW'
+    POST_TYPE_CHOICES = [
+        (ARTICLE, 'Article'),
+        (NEWS, 'News'),
+    ]
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='posts')
+    post_type = models.CharField(max_length=2, choices=POST_TYPE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField('Category', through='PostCategory')
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    rating = models.FloatField(default=0)
+
+    def preview(self):
+        if len(self.content) > 124:
+            return f'{self.content[:123]} ...'
+        return self.content
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    rating = models.FloatField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
